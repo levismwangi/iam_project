c8d81f3b3e2f9c8356c447fd633415754c5b14af
levis4dev <levisnjuguna648@gmail.com>
2026-04-09 18:57:27 +0300

    fix: added terraform block to users module

--- modules/users/main.tf
+++ modules/users/main.tf
@@ -19,3 +19,13 @@ resource "azuread_user" "this" {
     ignore_changes = [password]
   }
 }
+
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.47"
+    }
+  }
+}
