a5b60003b88a4ea69ecef85d544d5cfa60f55e85
levis4dev <levisnjuguna648@gmail.com>
2026-04-09 18:48:35 +0300

    fix: added terraform block to groups module

--- modules/groups/main.tf
+++ modules/groups/main.tf
@@ -34,3 +34,13 @@ resource "azuread_group" "break_glass" {
   security_enabled = true
   mail_enabled     = false
 }
+
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.47"
+    }
+  }
+}
