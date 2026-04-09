b9a0fef4796c5ee536d2033b48527d500b6b6997
levis4dev <levisnjuguna648@gmail.com>
2026-04-09 20:59:51 +0300

    fix: added use_oidc =true for the providers

--- providers.tf
+++ providers.tf
@@ -26,9 +26,11 @@ terraform {
 
 provider "azuread" {
   tenant_id = var.tenant_id
+  use_oidc  = true
 }
 
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
+  use_oidc        = true
 }
